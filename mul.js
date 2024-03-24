02681a7fdf2b339eedcaaff319c2bda78978e454
Abdulrahman Khalid <dahmbama@gmail.com>
2024-03-25 02:05:05 +0300

    finished implementing the lambda functions and the api gateway

---
+++ mul.js
@@ -0,0 +1,12 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('add', (req, res) => {
+  const x = Number(req.query.X)
+  const y = Number(req.query.Y)
+  const result = x * y
+  res.send(JSON.stringify({
+    "X": x,
+    "Y": y,
+    "Result": result
+  }));
+});
